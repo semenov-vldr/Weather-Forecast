cd93c342ffc8a7de002de978f5a85c3a1544a4b5
Sergei Rozhkov <s.rozhkov@andersenlab.com>
2020-10-26 10:04:41 +0300

    Revert "init"
    This reverts commit 5af3d9f1

--- js/components/big-card-empty.component.js
+++
@@ -1,9 +0,0 @@
-import {AbstractComponent} from "./abstract.component.js";
-
-export class BigCardEmptyComponent extends AbstractComponent {
-  _getTemplate() {
-    return `<li class="card">
-              <div class="big-card--empty"></div>
-            </li>`;
-  }
-}
